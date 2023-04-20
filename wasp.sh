a4d7dc097f6e54c6e2b930b637b66e4c4ce8dd0f
Rudix <rudix.lab@gmail.com>
2023-04-20 17:30:33 +0300

    xxx

--- wasp.sh
+++ wasp.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 
-# Get the list of changed files
-files=$(git diff-tree --no-commit-id --name-only -r HEAD)
-echo $files
+# Get the list of changed files and save to a file
+git diff-tree --no-commit-id --name-only -r HEAD > changed_files.txt
+
 # Loop through each file and find the second directory path
 declare -A dirs
-for file in $files; do dir=$(echo "$file" | cut -d'/' -f2)  ((dirs[$dir]++)) done
+while read -r file; do
+  # Get the second directory path
+  dir=$(echo "$file" | cut -d'/' -f2)
+  # Increment the count for this directory
+  ((dirs[$dir]++))
+done < changed_files.txt
 
 # Find the directory with the highest count
 max_count=0
@@ -19,8 +24,14 @@ for dir in "${!dirs[@]}"; do
 done
 
 # Display the directory with the highest count
-echo $max_dir
-cd ./apps/$max_dir
+if [[ -n $max_dir ]]; then
+  echo "Directory with the highest count: /example/$max_dir/"
+else
+  echo "No changes found."
+fi
+# Display the directory with the highest count
+echo ${max_dir}
+cd ./apps/${max_dir}
 pwd
 ls -la 
 npm install
