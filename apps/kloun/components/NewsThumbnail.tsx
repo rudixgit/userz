49af48c8090c146b903f1676245eb7c762250b5e
Radoslav Georgiev <rudix.lab@gmail.com>
2023-04-24 14:04:14 +0000

    xxx

--- apps/kloun/components/NewsThumbnail.tsx
+++ apps/kloun/components/NewsThumbnail.tsx
@@ -25,16 +25,9 @@ const NewsThumbnail = ({
       className="newswrap"
     >
       <div className="flex items-center">
-        <div className="absolute bg-gradient-to-t from-gray-900 to-transparent pl-2 text-xs h-full flex items-end" style={{ width: 120 }}>
-          {date && (
-            <div className='-mt-2'>
-              <TimeAgo
-                date={new Date(date).toISOString()}
-                formatter={formatter}
-              />
-            </div>
-          )}</div>
-        <div className="flex items-center justify-center">
+
+        <div className="flex items-center justify-center relative">
+
           {image && (
             <img
               alt={title}
@@ -42,6 +35,14 @@ const NewsThumbnail = ({
               loading="lazy"
             />
           )}
+          <img src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==" alt="" width={120} height={180} style={{ width: 120, height: 180 }} className="bg-gradient-to-t from-gray-900 to-transparent pl-2 text-xs h-full   absolute left-0" />
+          <div className="absolute flex items-end left-0 text-xs p-2" style={{ width: 120, height: 180 }}>
+            {date && (
+              <TimeAgo
+                date={new Date(date).toISOString()}
+                formatter={formatter}
+              />
+            )}</div>
           <h3 className="px-2 font-bold text-slate-300 dark:text-gray-800">
             {title}
           </h3>
