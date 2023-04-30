b68b4e884e97ffa239b6692228e8b462d666b674
Radoslav Georgiev <rudix.lab@gmail.com>
2023-04-30 13:18:13 +0300

    xxx

--- apps/clown/src/pages/api/news/[id].jpg.ts
+++ apps/clown/src/pages/api/news/[id].jpg.ts
@@ -3,20 +3,25 @@ import sharp from 'sharp';
 import db from "../../../../../kloun/data/client";
 export async function get({ params }: { params: { id: string } }) {
     const { id } = params;
-    const data = await db.view("newsbg/news", {
-        reduce: false,
-        limit: 1,
-        key: id,
-        update: "lazy",
-    });
-    const response = await fetch(data.image);
-    const buffer = Buffer.from(await response.arrayBuffer());
-    const resizedBuffer = await sharp(buffer)
-        .resize({ height: 180 })
-        .toBuffer();
+    try {
+        const data = await db.view("newsbg/news", {
+            reduce: false,
+            limit: 1,
+            key: id,
+            update: "lazy",
+        });
+        const response = await fetch(data.image);
+        const buffer = Buffer.from(await response.arrayBuffer());
+        const resizedBuffer = await sharp(buffer)
+            .resize({ height: 180 })
+            .toBuffer();
 
-    return {
-        body: resizedBuffer,
-        encoding: 'binary',
-    };
+        return {
+            body: resizedBuffer,
+            encoding: 'binary',
+        };
+    } catch (error) {
+        console.log(error);
+    }
+
 }
