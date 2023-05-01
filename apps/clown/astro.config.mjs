678907ed8b328f1243f2f837f518f9828cc0b070
Radoslav Georgiev <rudix.lab@gmail.com>
2023-05-01 15:13:48 +0300

    xxx

--- apps/clown/astro.config.mjs
+++ apps/clown/astro.config.mjs
@@ -1,14 +1,11 @@
 import cloudflare from '@astrojs/cloudflare'
 import tailwind from '@astrojs/tailwind'
 import { defineConfig } from 'astro/config'
-// https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: cloudflare({ mode: 'advanced' }),
   integrations: [
     tailwind({
-      // Example: Disable injecting a basic `base.css` import on every page.
-      // Useful if you need to define and/or import your own custom `base.css`.
       config: { applyBaseStyles: false }
     })
   ]
