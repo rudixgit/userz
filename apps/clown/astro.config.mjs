c1010eeed6a611259a6aac113fac0c6549bc4915
Radoslav Georgiev <rudix.lab@gmail.com>
2023-05-02 21:40:51 +0300

    xxx

--- apps/clown/astro.config.mjs
+++ apps/clown/astro.config.mjs
@@ -1,10 +1,12 @@
 import cloudflare from '@astrojs/cloudflare'
+import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
   output: 'server',
   integrations: [
+    react(),
     tailwind({
       config: { applyBaseStyles: false }
     })
