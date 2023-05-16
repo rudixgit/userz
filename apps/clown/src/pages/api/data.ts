f2707e7538aae44c81545185dcdfd483ca239107
Radoslav Georgiev <rudix.lab@gmail.com>
2023-05-16 08:18:22 +0000

    xxx

--- apps/clown/src/pages/api/data.ts
+++ apps/clown/src/pages/api/data.ts
@@ -1,10 +1,14 @@
 import type { APIRoute } from 'astro';
 
 export const get: APIRoute = ({ request }) => {
-	return {
-		body: JSON.stringify({
-			path: new URL(request.url).pathname,
-			date: new Date().toISOString()
-		})
-	};
+	return new Response(JSON.stringify({
+		path: new URL(request.url).pathname,
+		date: new Date().toISOString()
+	}), {
+		status: 200,
+		headers: {
+			"Content-Type": "application/json"
+		}
+	});
+
 }
