a0679a2b3f32b32f03be8081cfe35ab9d2e8494b
Radoslav Georgiev <rudix.lab@gmail.com>
2023-05-16 10:31:50 +0000

    xxx

--- apps/clown/src/pages/api/data.ts
+++ apps/clown/src/pages/api/data.ts
@@ -1,5 +1,6 @@
 import type { APIRoute } from 'astro';
 const cacheControl = 'public, max-age=3600';
+const expires = new Date(Date.now() + 3600 * 1000).toUTCString(); // cache for 1 hour
 export const get: APIRoute = ({ request }) => {
 	return new Response(JSON.stringify({
 		path: new URL(request.url).pathname,
@@ -8,8 +9,9 @@ export const get: APIRoute = ({ request }) => {
 		status: 200,
 		headers: {
 			'Cache-Control': cacheControl,
+			'Expires': expires,
 			"Content-Type": "application/json"
 		}
 	});
 
-}
+} 
