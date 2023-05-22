0fb8cc08b62b0bb8746a938e25eb4cf37cbd8ad9
Radoslav Georgiev <rudix.lab@gmail.com>
2023-05-22 23:35:51 +0300

    xxxx

--- apps/kloun/src/pages/api/cache.ts
+++ apps/kloun/src/pages/api/cache.ts
@@ -12,15 +12,23 @@ async function fetchWithCloudflare(url: string, options: CloudflareFetchOptions)
 	return fetch(url, options);
 }
 
+function convertToFriendlierId(url: string): string {
+	const friendlyId = url
+		.replace(/[^\w\s]/gi, '') // Remove special characters
+		.replace(/\s+/g, '-') // Replace spaces with hyphens
+		.toLowerCase(); // Convert to lowercase
 
+	return friendlyId;
+}
 export const get: APIRoute = async function get({ request }: APIContext) {
 	const url = request.url.split('?url=')[1]
 	const ttl = url ? Number(url.split('&cache=')[1]) : 5
+
 	let response = await fetchWithCloudflare(url, {
 		cf: {
 			cacheTtl: ttl,
 			cacheEverything: true,
-			cacheKey: url,
+			cacheKey: convertToFriendlierId(url),
 		},
 	});
 	return new Response(response.body, {
