138000a690bb1f415c5432153b4f2e3df98c20b0
Radoslav Georgiev <rudix.lab@gmail.com>
2023-05-23 00:15:13 +0300

    xxx

--- apps/kloun/src/pages/api/cache.ts
+++ apps/kloun/src/pages/api/cache.ts
@@ -31,12 +31,5 @@ export const get: APIRoute = async function get({ request }: APIContext) {
 			cacheKey: convertToFriendlierId(url),
 		},
 	});
-	return new Response(response.body, {
-		status: 200,
-		headers: {
-			"Content-Type": "application/json",
-			"Cache-Control": "public, max-age=" + ttl.toString()
-		}
-	});
-
+	return new Response(response.body);
 }
