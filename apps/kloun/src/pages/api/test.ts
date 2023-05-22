8368e2570b8a86996d09b213130e3f4a8b338701
Radoslav Georgiev <rudix.lab@gmail.com>
2023-05-22 19:41:44 +0300

    xxx

--- apps/kloun/src/pages/api/test.ts
+++ apps/kloun/src/pages/api/test.ts
@@ -1,12 +1,23 @@
 import type { APIRoute } from "astro";
 export const prerender = false
 
+interface CloudflareFetchOptions extends RequestInit {
+	cf?: {
+		cacheTtl?: number;
+		cacheEverything?: boolean;
+		cacheKey?: string;
+	};
+}
+async function fetchWithCloudflare(url: string, options: CloudflareFetchOptions): Promise<Response> {
+	return fetch(url, options);
+}
+
 
 export const get: APIRoute = async function get() {
 	const someCustomKey = `https://baconipsum.com/api/?type=meat-and-filler`;
-	let response = await fetch(someCustomKey, {
+	let response = await fetchWithCloudflare(someCustomKey, {
 		cf: {
-			cacheTtl: 15,
+			cacheTtl: 5,
 			cacheEverything: true,
 			cacheKey: someCustomKey,
 		},
