4ad5adc30a08f8daf3f5fa6463c3c9e27c13dd36
Radoslav Georgiev <rudix.lab@gmail.com>
2023-05-28 13:25:38 +0300

    xxxxxx

--- apps/kloun/astro.config.mjs
+++ apps/kloun/astro.config.mjs
@@ -1,20 +1,16 @@
 import cloudflare from '@astrojs/cloudflare';
-
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   output: "hybrid",
   integrations: [
     react(),
-
     tailwind({
       config: { applyBaseStyles: false }
     })
   ],
-  compressHTML: true,
   adapter: cloudflare({ mode: 'advanced' }),
   experimental: {
     inlineStylesheets: "never",
