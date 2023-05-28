b0d40b5e3e0a3b8e39c486b3975edab10d6f9610
Radoslav Georgiev <rudix.lab@gmail.com>
2023-05-28 17:14:52 +0300

    bots

--- apps/kloun/src/middleware.ts
+++ apps/kloun/src/middleware.ts
@@ -1,6 +1,33 @@
-export function onRequest({ locals, request }: { locals: { url: string }, request: { url: string } }, next: () => void) {
+const userAgents = [
+	'Mozilla', // Common string for multiple browsers
+	'Chrome', // Chrome browser
+	'Firefox', // Firefox browser
+	'Safari', // Safari browser
+	'Edg', // Microsoft Edge browser
+	'Opera', // Opera browser
+	'Trident', // Internet Explorer browser
+	'AppleWebKit', // WebKit-based browsers
+	'Googlebot',
+	'AdsBot',
+	'Mediapartners',
+	'Bingbot', // Bingbot crawler
+	'Slurp', // Yahoo Slurp crawler
+	'DuckDuckBot', // DuckDuckGo crawler
+	'Baiduspider', // Baidu spider
+	'YandexBot' // Yandex crawler // Googlebot crawler
+];
 
+export function onRequest({ locals, request }: { locals: { url: string }, request: { url: string, headers: { get: (x: string) => string } } }, next: () => void) {
+	const userAgentHeader = request.headers.get('user-agent');
+	const userAgent = userAgentHeader ? userAgentHeader : 'Unknown';
+	console.log('User Agent:', userAgent);
 	locals.url = request.url
-
-	return next()
+	const isSupportedBrowser = userAgents.some(agent => userAgent.includes(agent));
+	if (isSupportedBrowser) {
+		return next()
+	} else {
+		return new Response('Method not Allowed', {
+			status: 405,
+		});
+	}
 };
