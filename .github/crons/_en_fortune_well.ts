7ecdc3b2927fe58a5bd5ec0611450c7efccbf4c0
Radoslav Georgiev <rudix.lab@gmail.com>
2023-06-07 22:40:37 +0300

    xxx

--- .github/crons/_en_fortune_well.ts
+++ .github/crons/_en_fortune_well.ts
@@ -17,23 +17,9 @@ const go = async () => {
 
 	return links;
 };
-const go2 = async () => {
-	const response = await fetch("https://fortune.com/section/europe/", {
-		method: "GET",
-		headers: scrheaders,
-	});
-	const d = await response.text();
-	const links1 = Array.from(new JSDOM(d).window.document.querySelectorAll("a"))
-		.map((link: HTMLElement) => link.getAttribute("href"))
-		.filter((href) => href !== null && href.includes('/20') && href.split('-').length > 2).sort() as string[];
-	const links = getUniqueStrings(links1)
-	console.log(links)
-	await Promise.all(links.map((link) => scrapeArticle(link, ["Снимка: "], "NewsENProcess")));
 
-	return links;
-};
 // scrapeArticle('https://www.buzzfeed.com/leylamohammed/kris-jenner-pursuit-of-fame-cost-the-kardashians?origin=x1x', ['xxx'], "TestEN").then(() => console.log('done'));
 
 go().then((links) => console.log(links.length));
 
-go2().then((links) => console.log(links.length));
+
